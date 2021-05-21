48924dc9ccf9cf23c58f5074e6c1796415f97a54
Adithya Vijay K <adithyavj@gmail.com>
2021-05-21 22:19:37 +0530

    Adding attributes to validate the properties of Model Employee.
    Added different attributes for EmployeeModel
    properties for proper validation and Display
    Also made small changes in bootstrap.css (for label alignment)

--- MVCApp/Models/EmployeeModel.cs
+++ MVCApp/Models/EmployeeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,39 @@ namespace MVCApp.Models
 {
     public class EmployeeModel
     {
+        // Adding attributes to validate the properties of Model Employee
+
+        // We need to add some conditions to employeeid (min-max range) , Also change Display name of EmployeeId to Employee ID
+        [Display(Name = "Employee ID")]
+        [Range(100000, 999999, ErrorMessage = "Enter a Valid Employee Id")]
         public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="You need to give us your First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You need to give us your Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "You need to give us your Email address")]
         public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email")]
+        [Compare("EmailAddress",ErrorMessage ="The email and confirm email must match")]
         public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(100,MinimumLength =10,ErrorMessage ="Provide a long enough password")]
+        [Required(ErrorMessage ="You must have a password")]
         public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirm password do not match")]
         public string ConfirmPassword { get; set; }
 
     }
