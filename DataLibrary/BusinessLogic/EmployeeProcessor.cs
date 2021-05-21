24effc0cf5554fdcc24ddfc2c3909a199d6a0735
Adithya Vijay K <adithyavj@gmail.com>
2021-05-21 23:52:33 +0530

    Add dapper for ORM
    Adding dapper nuget package in ClassLibrary
    for easy ORM-Mapping and data access.
    - methods for get,save data in SqlDataAccess
    - class in business logic for create,load
       employees

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,42 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    // We make this static because we don't need to store data here
+    public static class EmployeeProcessor
+    {
+        // Create Employee
+        public static int CreateEmployee(int employeeId, string firstName,
+            string lastName, string emailAddress)
+        {
+            EmployeeModel data = new EmployeeModel
+            {
+                EmployeeId = employeeId,
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress
+            };
+
+            // Insert data to table using parameterised query
+            string sql = @"insert into dbo.Employee(EmployeeId, FirstName, LastName, EmailAddress)
+                           values (@EmployeeId, @FirstName, @LastName, @EmailAddress)";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = @"select Id, EmployeeId, FirstName, LastName, EmailAddress 
+                           from dbo.Employees";
+
+            // This Return EmployeeModel <T>
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+    }
+}
--- DataLibrary/DataAccess/SqlDataAccess.cs
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataLibrary.DataAccess
 {
-    public class SqlDataAccess
+    public static class SqlDataAccess
     {
+        // Method to GetSQL Connection
+        public static string GetConnectionString(string connectionName = "MVCDB")
+        {
+            // takes argument and pass to this 
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
 
+
+        // Load Data - T represents the model to which we want to load data
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                // This returns Ienumerable, but we convert it to List and Return List<T> - List of Model
+                return connection.Query<T>(sql).ToList();
+            }
+        }
+
+        // This saves one model ie, T - data
+        public static int SaveData<T>(string sql,T data)
+        {
+            using (IDbConnection connection=new SqlConnection(GetConnectionString()))
+            {
+                // saves data to DB
+                return connection.Execute(sql, data);
+            }
+        }
     }
 }
