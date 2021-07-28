7f81528fed30517b72c761c6db47f67fe9281a04
Adithya Vijay K <adithyav@qburst.com>
2021-07-28 20:57:20 +0530

    Add ViewBags,ViewData,TempData etc

---
+++ MVCApp/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using MVCApp.Models;
+using MVCApp.Models.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentBusinessLayer studentBusinessLayer = new StudentBusinessLayer();
+            StudentModel student = studentBusinessLayer.GetStudentsById(1);
+
+            ViewData["Student"] = student;
+            ViewData["Header"] = "Student Details";
+
+            return View();
+        }
+
+        public ActionResult Method1()
+        {
+            TempData["Name"] = "Pranaya";
+            TempData["Age"] = 30;
+            return View();
+        }
+
+        public ActionResult Method2()
+        {
+            string Name;
+            int Age;
+            if (TempData.ContainsKey("Name"))
+                Name = TempData["Name"].ToString();
+            if (TempData.ContainsKey("Age"))
+                Age = int.Parse(TempData["Age"].ToString());
+            // do something with userName or userAge here 
+            return View();
+        }
+
+        public ActionResult Method3()
+        {
+            string Name;
+            int Age;
+            if (TempData.ContainsKey("Name"))
+                Name = TempData["Name"].ToString();
+            if (TempData.ContainsKey("Age"))
+                Age = int.Parse(TempData["Age"].ToString());
+            // do something with userName or userAge here 
+            return View();
+        }
+    }
+}
---
+++ MVCApp/Models/BusinessLogic/StudentBusinessLayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models.BusinessLogic
+{
+    public class StudentBusinessLayer
+    {
+        public StudentModel GetStudentsById(int id)
+        {
+            StudentModel student = new StudentModel()
+            {
+                EmployeeId = id,
+                Name = "Adithya",
+                Address = "Baby Villa",
+                City = "Melattur",
+                Gender = "Male",
+                Salary = 50000
+            };
+            return student;
+        }
+    }
+}
